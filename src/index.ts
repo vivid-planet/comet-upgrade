8eaddbec3e7f7afb36f712b60612db7dfd0c9132
Johannes Obermair <johannes.obermair@vivid-planet.com>
2023-01-09 16:51:18 +0100

    Get current version

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,58 @@
 import fs from "fs";
 import path from "path";
+import semver from "semver";
 
 const VERSION_NUMBER = /^v?\d+$/;
 
 async function main() {
-    let targetVersion = process.argv[2];
+    let targetVersionArg = process.argv[2];
 
-    if (targetVersion === undefined) {
+    if (targetVersionArg === undefined) {
         console.error("Missing target version! Usage: npx @comet/upgrade <version>");
         process.exit(-1);
     }
 
-    if (!VERSION_NUMBER.test(targetVersion)) {
+    if (!VERSION_NUMBER.test(targetVersionArg)) {
         console.error("Can't parse version number. Example usage: npx @comet/upgrade v4");
         process.exit(-1);
     }
 
-    if (!targetVersion.startsWith("v")) {
-        targetVersion = `v${targetVersion}`;
+    if (targetVersionArg.startsWith("v")) {
+        targetVersionArg = targetVersionArg.substring(1);
     }
 
-    const scriptsFolder = path.join(__dirname, targetVersion);
+    const targetVersion = Number(targetVersionArg);
+
+    const scriptsFolder = path.join(__dirname, `v${targetVersion}`);
 
     if (!fs.existsSync(scriptsFolder)) {
-        console.error(`Can't find target version '${targetVersion}'`);
+        console.error(`Can't find target version 'v${targetVersionArg}'`);
         listTargetVersions();
         process.exit(-1);
     }
 
+    const currentVersion = getCurrentVersion();
+
+    console.info(`Upgrading from v${currentVersion} to v${targetVersion}`);
     await runUpgradeScripts(targetVersion);
 }
 
-async function runUpgradeScripts(targetVersion: string) {
-    const scriptsFolder = path.join(__dirname, targetVersion);
+function getCurrentVersion() {
+    if (!fs.existsSync("admin/node_modules/@comet/admin/package.json")) {
+        return;
+    }
+
+    const packageJson = JSON.parse(fs.readFileSync("admin/node_modules/@comet/admin/package.json").toString());
+
+    if (!packageJson.version || !semver.valid(packageJson.version)) {
+        return;
+    }
+
+    return semver.major(packageJson.version);
+}
+
+async function runUpgradeScripts(targetVersion: number) {
+    const scriptsFolder = path.join(__dirname, `v${targetVersion}`);
 
     for (const fileName of fs.readdirSync(scriptsFolder)) {
         const upgradeScript = await import(path.join(scriptsFolder, fileName));
@@ -40,7 +60,7 @@ async function runUpgradeScripts(targetVersion: string) {
         try {
             await upgradeScript.default();
         } catch (error) {
-            console.error(`Script '${targetVersion}/${fileName}' failed to execute. See original error below`);
+            console.error(`Script 'v${targetVersion}/${fileName}' failed to execute. See original error below`);
             console.error(error);
         }
     }
