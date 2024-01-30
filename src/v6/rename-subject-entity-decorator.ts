48d05d42ac48ec4f1d55ac0089d2e0800e07c1b3
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-01-30 09:45:07 +0100

    Add upgrade script to rename `@SubjectEntity()` decorator

---
+++ src/v6/rename-subject-entity-decorator.ts
@@ -0,0 +1,26 @@
+import { ESLint } from "eslint";
+import { readFile, writeFile } from "fs/promises";
+import { glob } from "glob";
+
+/**
+ * Renames @SubjectEntity() decorator to @AffectedEntity() decorator.
+ */
+export default async function renameSubjectEntityDecorator() {
+    const eslint = new ESLint({ cwd: process.cwd(), fix: true });
+
+    const files: string[] = glob.sync(["api/src/**/*.ts"]);
+
+    for (const filePath of files) {
+        console.log(filePath);
+        let fileContent = (await readFile(filePath)).toString();
+
+        if (!fileContent.includes("SubjectEntity")) {
+            continue;
+        }
+
+        fileContent = fileContent.replace(/SubjectEntity/g, "AffectedEntity");
+        const lintResult = await eslint.lintText(fileContent, { filePath });
+        const lintOutput = lintResult[0] && lintResult[0].output ? lintResult[0].output : lintResult[0].source;
+        await writeFile(filePath, lintOutput ?? fileContent);
+    }
+}
