7becb4b5894bde495aa1bf73e7794ed1a4e73bc8
Thomas Dax <thomas.dax@vivid-planet.com>
2024-04-11 09:56:05 +0200

    Extract executeCommand

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { spawn } from "child_process";
 import fs from "fs";
 import path from "path";
 import semver from "semver";
+
+import { executeCommand } from "./util/execute-command.util";
 
 const VERSION_NUMBER = /^v?\d+$/;
 const microservices = ["api", "admin", "site"] as const;
@@ -129,23 +130,6 @@ async function updateDependencies(targetVersion: number) {
             ...devDependencies.map((dependency) => `${dependency}@${targetVersion}`),
         ]);
     }
-}
-
-// Inspired by https://github.com/facebook/create-react-app/blob/main/packages/create-react-app/createReactApp.js#L383
-function executeCommand(command: string, args: string[] = []) {
-    return new Promise<void>((resolve, reject) => {
-        const child = spawn(command, args, { stdio: "inherit" });
-
-        child.on("close", (code) => {
-            if (code !== 0) {
-                reject({
-                    command: `${command} ${args.join(" ")}`,
-                });
-                return;
-            }
-            resolve();
-        });
-    });
 }
 
 async function runUpgradeScripts(targetVersion: number) {
