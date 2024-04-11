bb241575cff46b3f1dce0aff184cfaf7ac4c75c9
Thomas Dax <t.dax@gmx.net>
2024-04-11 15:11:34 +0200

    v7: Import admin theme types in vendors.d.ts (#10)
    Enables the usage of custom theme extensions like the highlight colors
    from https://github.com/vivid-planet/comet/pull/1840

---
+++ src/v7/import-admin-theme-types.ts
@@ -0,0 +1,27 @@
+import fs from "fs";
+import { readFile, writeFile } from "fs/promises";
+
+import { formatCode } from "../util/format-code.util";
+
+/**
+ * Imports types from `@comet/admin-theme` in vendors.d.ts to allow using custom theme variants and colors
+ */
+export default async function importAdminThemeTypes() {
+    const filePath = "admin/src/vendors.d.ts";
+    const typeReference = '/// <reference types="@comet/admin-theme" />';
+
+    let fileContent = "";
+    if (fs.existsSync(filePath)) {
+        fileContent = (await readFile(filePath)).toString();
+    }
+
+    if (fileContent.includes(typeReference)) {
+        return;
+    }
+
+    fileContent = `${typeReference}
+    
+    ${fileContent}`;
+
+    await writeFile(filePath, await formatCode(fileContent, filePath));
+}
