86669c6e7c5d0e5dca9e70cf87de4a9b5f4fdc10
Daniel Karnutsch <daniel.karnutsch@vivid-planet.com>
2024-06-25 16:51:53 +0200

    Add Update Script for renaming DateFilter to DateTimeFilter

---
+++ src/v7/rename-date-filter-to-date-time-filter.ts
@@ -0,0 +1,20 @@
+import { readFile, writeFile } from "fs/promises";
+import { glob } from "glob";
+
+import { formatCode } from "../util/format-code.util";
+
+// Renames DateFilter to DateTimeFilter
+export default async function renameDateFilterToDateTimeFilter() {
+    const files: string[] = glob.sync(["api/src/**/*.ts"]);
+
+    for (const filePath of files) {
+        let fileContent = (await readFile(filePath)).toString();
+
+        if (!fileContent.includes("DateFilter")) {
+            continue;
+        }
+
+        fileContent = fileContent.replace(/DateFilter/g, "DateTimeFilter");
+        await writeFile(filePath, await formatCode(fileContent, filePath));
+    }
+}
