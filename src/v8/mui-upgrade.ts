00bfbf2eea89b2def42ceacb0f892e9de0f60243
Ricky Smith <jamesricky@me.com>
2024-10-11 19:38:36 +0200

    Only update MUI version if the required package.json file exists

--- src/v8/mui-upgrade.ts
+++ src/v8/mui-upgrade.ts
@@ -1,12 +1,20 @@
+import { existsSync } from "fs";
+
 import { executeCommand } from "../util/execute-command.util";
 import { updateDependencyVersion } from "../util/update-dependency-version.util";
 
+const adminPackageJsonPath = "admin/package.json";
+
 export default async function updateMuiVersion() {
-    await updateDependencyVersion("admin/package.json", "@mui/material", "^6.0.0");
-    await updateDependencyVersion("admin/package.json", "@mui/system", "^6.0.0");
-    await updateDependencyVersion("admin/package.json", "@mui/utils", "^6.0.0");
-    await updateDependencyVersion("admin/package.json", "@mui/icons-material", "^6.0.0");
-    await updateDependencyVersion("admin/package.json", "@mui/lab", "^6.0.0-beta.10");
+    if (!existsSync(adminPackageJsonPath)) {
+        return;
+    }
+
+    await updateDependencyVersion(adminPackageJsonPath, "@mui/material", "^6.0.0");
+    await updateDependencyVersion(adminPackageJsonPath, "@mui/system", "^6.0.0");
+    await updateDependencyVersion(adminPackageJsonPath, "@mui/utils", "^6.0.0");
+    await updateDependencyVersion(adminPackageJsonPath, "@mui/icons-material", "^6.0.0");
+    await updateDependencyVersion(adminPackageJsonPath, "@mui/lab", "^6.0.0-beta.10");
 
     await executeCommand("npm", ["install", "--prefix", "admin", "--no-audit", "--loglevel", "error"]);
 
