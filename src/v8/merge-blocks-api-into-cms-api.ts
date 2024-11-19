cc820d073185c530e161246bb8bcfdc6296eb1a8
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2024-11-19 08:12:31 +0100

    Add upgrade script to merge `@comet/blocks-api` into `@comet/cms-api` (#24)
    Upgrade script for https://github.com/vivid-planet/comet/pull/2635.

---
+++ src/v8/merge-blocks-api-into-cms-api.ts
@@ -0,0 +1,63 @@
+import { readFile, writeFile } from "fs/promises";
+import { glob } from "glob";
+import { Project } from "ts-morph";
+
+const renamedExports = {
+    getMostSignificantPreviewImageUrlTemplate: "getMostSignificantPreviewImageUrlTemplateFromBlock",
+    getPreviewImageUrlTemplates: "getPreviewImageUrlTemplatesFromBlock",
+    getSearchText: "getSearchTextFromBlock",
+    inputToData: "blockInputToData",
+    TransformResponse: "TransformBlockResponse",
+    TransformResponseArray: "TransformBlockResponseArray",
+    transformToSave: "transformToBlockSave",
+    transformToSaveIndex: "transformToBlockSaveIndex",
+    TraversableTransformResponse: "TraversableTransformBlockResponse",
+    TraversableTransformResponseArray: "TraversableTransformBlockResponseArray",
+    typesafeMigrationPipe: "typeSafeBlockMigrationPipe",
+};
+
+export default async function mergeBlocksApiIntoCmsApi() {
+    const project = new Project({ tsConfigFilePath: "./api/tsconfig.json" });
+    const files: string[] = glob.sync(["api/src/**/*.ts"]);
+
+    for (const filePath of files) {
+        const sourceFile = project.getSourceFile(filePath);
+
+        if (!sourceFile) {
+            throw new Error(`Can't get source file for ${filePath}`);
+        }
+
+        const blocksApiImport = sourceFile.getImportDeclaration((declaration) => declaration.getModuleSpecifierValue().includes("@comet/blocks-api"));
+
+        if (!blocksApiImport) {
+            continue;
+        }
+
+        const blocksApiImports = blocksApiImport.getNamedImports().map((namedImport) => namedImport.getText());
+
+        const importsToRename = Object.entries(renamedExports).filter(([oldExport]) => blocksApiImports.includes(oldExport));
+
+        blocksApiImport.remove();
+
+        const cmsApiImport = sourceFile.getImportDeclaration((declaration) => declaration.getModuleSpecifierValue().includes("@comet/cms-api"));
+
+        if (cmsApiImport) {
+            cmsApiImport.addNamedImports(blocksApiImports);
+        } else {
+            sourceFile.addImportDeclaration({
+                namedImports: blocksApiImports,
+                moduleSpecifier: "@comet/cms-api",
+            });
+        }
+
+        await sourceFile.save();
+
+        let fileContent = (await readFile(filePath)).toString();
+
+        for (const [oldExport, newExport] of importsToRename) {
+            fileContent = fileContent.replaceAll(oldExport, newExport);
+        }
+
+        await writeFile(filePath, fileContent);
+    }
+}
