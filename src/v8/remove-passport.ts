d99ce0ab9eb451ebba6e0b1e346fcfcf21cce486
Franz Unger <franz.unger@vivid-planet.com>
2025-01-14 11:33:22 +0100

    Add script to remove Passport (#42)
    Co-authored-by: Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>

---
+++ src/v8/remove-passport.ts
@@ -0,0 +1,22 @@
+import { existsSync } from "fs";
+
+import { PackageJson } from "../util/package-json.util";
+
+export const stage = "before-install";
+
+export default async function removePassport() {
+    if (!existsSync("api/package.json")) {
+        return;
+    }
+
+    const packageJson = new PackageJson("api/package.json");
+    packageJson.addDependency("@nestjs/jwt", "^10.2.0");
+    packageJson.removeDependency("@nestjs/passport");
+    packageJson.removeDependency("passport");
+    packageJson.removeDependency("passport-http");
+    packageJson.removeDependency("passport-http-bearer");
+    packageJson.removeDependency("@types/passport");
+    packageJson.removeDependency("@types/passport-http");
+    packageJson.removeDependency("@types/passport-http-bearer");
+    await packageJson.save();
+}
