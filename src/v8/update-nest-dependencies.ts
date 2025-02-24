9dee010f44186f8c79bcbdd56944bca4400fcec4
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2025-02-24 12:36:06 +0100

    Upgrade NestJS to v11 (#62)

--- src/v8/update-nest-dependencies.ts
+++ src/v8/update-nest-dependencies.ts
@@ -15,19 +15,22 @@ export default async function updateNestDependencies() {
     packageJson.removeDependency("apollo-server-core");
     packageJson.removeDependency("apollo-server-express");
 
-    packageJson.updateDependency("@nestjs/apollo", "^12.0.0");
-    packageJson.updateDependency("@nestjs/common", "^10.0.0");
-    packageJson.updateDependency("@nestjs/core", "^10.0.0");
-    packageJson.updateDependency("@nestjs/graphql", "^12.0.0");
-    packageJson.updateDependency("@nestjs/platform-express", "^10.0.0");
+    packageJson.updateDependency("@nestjs/apollo", "^13.0.3");
+    packageJson.updateDependency("@nestjs/common", "^11.0.10");
+    packageJson.updateDependency("@nestjs/core", "^11.0.10");
+    packageJson.updateDependency("@nestjs/graphql", "^13.0.3");
+    packageJson.updateDependency("@nestjs/mapped-types", "^2.1.0");
+    packageJson.updateDependency("@nestjs/platform-express", "^11.0.10");
+    packageJson.updateDependency("@nestjs/cli", "^11.0.3");
+    packageJson.updateDependency("@nestjs/schematics", "^11.0.1");
+    packageJson.updateDependency("@nestjs/testing", "^11.0.0");
 
-    packageJson.updateDependency("graphql", "^16.6.0");
+    packageJson.updateDependency("graphql", "^16.10.0");
+
+    packageJson.updateDependency("express", "^5.0.1");
+    packageJson.updateDependency("@types/express", "^5.0.0");
 
     packageJson.updateDependency("@golevelup/nestjs-discovery", "^4.0.0");
-
-    packageJson.updateDependency("@nestjs/cli", "^10.0.0");
-    packageJson.updateDependency("@nestjs/schematics", "^10.0.0");
-    packageJson.updateDependency("@nestjs/testing", "^10.0.0");
 
     packageJson.save();
 }
