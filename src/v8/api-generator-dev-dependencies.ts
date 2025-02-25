5d8df62cdf2d03d0369119e7da90a11e59e0b10f
Manuel Blum <mb@vivid-planet.com>
2025-02-25 09:08:03 +0100

    fetch latest 8 version to pin version to package.json

--- src/v8/api-generator-dev-dependencies.ts
+++ src/v8/api-generator-dev-dependencies.ts
@@ -1,5 +1,6 @@
 import { existsSync } from "fs";
 
+import { executeCommand } from "../util/execute-command.util";
 import { PackageJson } from "../util/package-json.util";
 
 export const stage = "before-install";
@@ -9,9 +10,12 @@ export const stage = "before-install";
  */
 export default async function updatePrettier() {
     if (existsSync("api/package.json")) {
-        console.log("🚀 api/package.json add @comet/api-generator-package to dev-dependencies");
+        const result = await executeCommand("npm", ["view", "@comet/admin@8", "version", "--json"]);
+        const versions = JSON.parse(result) as string[];
+        const version = versions[versions.length - 1];
+        console.log(`🚀 api/package.json add @comet/api-generator package with version ${version} to dev-dependencies`);
         const packageJson = new PackageJson("api/package.json");
-        packageJson.addDependency("@comet/api-generator", "^8.0.0", true);
+        packageJson.addDependency("@comet/api-generator", versions[versions.length - 1], true);
         packageJson.save();
     }
 }
