9081ee316a744b3f68050aaa813a474cd332a432
Thomas Dax <t.dax@gmx.net>
2025-03-10 14:29:19 +0100

    Update SWC dependencies (#78)

---
+++ src/v8/update-swc-dependencies.ts
@@ -0,0 +1,18 @@
+import { existsSync } from "fs";
+
+import { PackageJson } from "../util/package-json.util";
+
+export const stage = "before-install";
+
+export default async function updateSwcDependencies() {
+    if (!existsSync("admin/package.json")) {
+        return;
+    }
+
+    const packageJson = new PackageJson("admin/package.json");
+
+    packageJson.updateDependency("@vitejs/plugin-react-swc", "^3.8.0");
+    packageJson.updateDependency("@swc/plugin-emotion", "^8.7.2");
+
+    packageJson.save();
+}
