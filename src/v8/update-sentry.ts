1ccc458b7da3e42c6ee06091f1c7623773165dd1
Thomas Dax <t.dax@gmx.net>
2025-03-10 14:31:07 +0100

    Update Sentry (#75)

---
+++ src/v8/update-sentry.ts
@@ -0,0 +1,32 @@
+import { existsSync } from "fs";
+import { Project, SyntaxKind } from "ts-morph";
+
+import { PackageJson } from "../util/package-json.util";
+
+export const stage = "before-install";
+
+export default async function updateSentry() {
+    if (!existsSync("api/package.json")) {
+        return;
+    }
+
+    const packageJson = new PackageJson("api/package.json");
+    packageJson.updateDependency("@sentry/node", "^9.0.0");
+    packageJson.save();
+
+    const project = new Project({ tsConfigFilePath: "./api/tsconfig.json" });
+    const sourceFile = project.getSourceFile("api/src/main.ts");
+    if (!sourceFile) throw new Error("main.ts not found");
+
+    sourceFile.getDescendantsOfKind(SyntaxKind.ExpressionStatement).forEach((node) => {
+        if (node.getText() === "app.use(Sentry.Handlers.requestHandler());") {
+            node.replaceWithText(`Sentry.setupExpressErrorHandler(app);`);
+        } else if (node.getText() === "app.use(Sentry.Handlers.tracingHandler());") {
+            node.remove();
+        } else if (node.getText() === "app.use(Sentry.Handlers.errorHandler());") {
+            node.remove();
+        }
+    });
+
+    await sourceFile.save();
+}
