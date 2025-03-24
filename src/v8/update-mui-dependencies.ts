bb27d638d8b43e2628121221fd28cfde6b356bf4
Thomas Dax <thomas.dax@vivid-planet.com>
2025-03-24 13:07:03 +0100

    Only upgrade mui packages if they exist

--- src/v8/update-mui-dependencies.ts
+++ src/v8/update-mui-dependencies.ts
@@ -13,11 +13,11 @@ export default async function updateMuiDependencies() {
 
     const packageJson = new PackageJson(adminPackageJsonPath);
 
-    packageJson.addDependency("@mui/material", "^6.0.0");
-    packageJson.addDependency("@mui/system", "^6.0.0");
-    packageJson.addDependency("@mui/utils", "^6.0.0");
-    packageJson.addDependency("@mui/icons-material", "^6.0.0");
-    packageJson.addDependency("@mui/lab", "^6.0.0-beta.10");
+    if (packageJson.hasDependency("@mui/material")) packageJson.addDependency("@mui/material", "^6.0.0");
+    if (packageJson.hasDependency("@mui/system")) packageJson.addDependency("@mui/system", "^6.0.0");
+    if (packageJson.hasDependency("@mui/utils")) packageJson.addDependency("@mui/utils", "^6.0.0");
+    if (packageJson.hasDependency("@mui/icons-material")) packageJson.addDependency("@mui/icons-material", "^6.0.0");
+    if (packageJson.hasDependency("@mui/lab")) packageJson.addDependency("@mui/lab", "^6.0.0-beta.10");
 
     packageJson.save();
 }
