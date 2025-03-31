d520a90c930feff5224a9e33d0e40cad32044aa1
Thomas Dax <thomas.dax@vivid-planet.com>
2025-03-31 15:26:38 +0200

    Move maxSrcResolution from imgproxy to dam in comet-config.json

---
+++ src/v8/move-maxSrcResolution-in-comet-config.ts
@@ -0,0 +1,22 @@
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import prettier from "prettier";
+
+/**
+ * Move the maxSrcResolution property from imgproxy to dam in comet-config.json
+ */
+export default async function moveMaxSrcResolutionInCometConfig() {
+    const cometConfigPath = "api/src/comet-config.json";
+    if (existsSync(cometConfigPath)) {
+        const content = readFileSync(cometConfigPath, "utf-8");
+
+        const config = JSON.parse(content);
+
+        if (config.imgproxy && config.imgproxy.maxSrcResolution !== undefined) {
+            config.dam.maxSrcResolution = config.imgproxy.maxSrcResolution;
+            delete config.imgproxy.maxSrcResolution;
+        }
+
+        const prettierConfig = await prettier.resolveConfig(process.cwd());
+        writeFileSync(cometConfigPath, prettier.format(JSON.stringify(config), { ...prettierConfig, parser: "json" }));
+    }
+}
